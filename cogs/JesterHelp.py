4d65d8914ccfc1f4cde4a613a993beb6efdd8d04
sam <theshadowofsam@gmail.com>
2021-12-21 18:39:49 -0500

    added new cog JesterHelp with the intention of it becoming the home of the help command currently has VERY basic functionality

---
+++ cogs/JesterHelp.py
@@ -0,0 +1,36 @@
+"""
+Samuel Lee
+12/20/2021
+JesterHelp.py
+
+This is a cog for Jester.py, the discord bot
+it contains the Help command and all related functions/commands
+"""
+import discord
+from discord.ext import commands
+import config
+
+
+class JesterHelp(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+    @commands.command(aliases=["h"])
+    async def help(self, ctx):
+        print(self.bot.cogs)
+        response = ''
+        for cogname, cog in self.bot.cogs.items():
+            response += f"'{cogname}' has commands:\n"
+            commandlist = cog.get_commands()
+            for c in commandlist:
+                if len(c.aliases)>0:
+                    response += f"\t-{c.name} has aliases: {' '.join(c.aliases)}\n"
+                else:
+                    response += f"\t-{c.name}\n"
+        await ctx.author.send(response)
+        
+
+
+def setup(bot):
+    bot.add_cog(JesterHelp(bot))
